b2a45e41bdf22882d832033ea3e342972f231cf4
Mariano Daniel Del Pin <mdelpin@practia.global>
2020-10-02 12:02:22 -0300

    Cambios para registrar tiempos de carrera

--- TP4/EJ8/Atleta.java
+++ TP4/EJ8/Atleta.java
@@ -1,13 +1,13 @@
 package TP4.EJ8;
 
-import java.util.Random;
-
 public class Atleta implements Runnable {
 
     Testigo testigoCarrera;
+    long tiempoInicioCarrera;
 
-    public Atleta(Testigo _testigoCarrera) {
+    public Atleta(Testigo _testigoCarrera, long _tiempoInicioCarrera) {
         testigoCarrera = _testigoCarrera;
+        tiempoInicioCarrera = _tiempoInicioCarrera;
     }
 
     @Override
@@ -21,15 +21,22 @@ public class Atleta implements Runnable {
             
             System.out.println("El atleta " + Thread.currentThread().getName() + " toma testigo y empieza a correr"); 
             
-            Random r = new Random(System.currentTimeMillis()); // tiempo en el que empieza a correr
+            float tiempoCarrera = (float)(Math.random()*(11000-9000+1)+9000);
 
-            Thread.sleep(r.nextInt(9000) + 2000); // tiempo en hacer su carrera
+            Thread.sleep((long) tiempoCarrera); // tiempo en hacer su carrera
 
             System.out.println("El atleta " + Thread.currentThread().getName() + " termina de correr y entrega testigo");
             
+            long tiempoFinCarrera = System.currentTimeMillis(); // tiempo en el que empieza a correr
+
+            long tiempoTotalCarrera = tiempoFinCarrera - tiempoInicioCarrera;
+
+            System.out.println("Tiempo " + Thread.currentThread().getName() + ": " + (long)tiempoCarrera/1000 + " seg");
+    
+            System.out.println("Tiempo de carrera : " + tiempoTotalCarrera/1000 + " seg");
+
             testigoCarrera.entregarTestigo(); // finaliza su carrera y entrega el testigo
-
-
+            
         } catch (InterruptedException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
--- TP4/EJ8/Carrera.java
+++ TP4/EJ8/Carrera.java
@@ -7,11 +7,15 @@ public class Carrera {
         Testigo postaCarrera = new Testigo();
         Thread[] Atletas = new Thread[4];
 
+        long tiempoInicioCarrera = System.currentTimeMillis(); // tiempo en el que empieza la carrera
+
         for(int i = 0; i < Atletas.length; i++) {
             int numeroCorredor = i + 1;
-            Atletas[i] = new Thread(new Atleta(postaCarrera),"Corredor " + numeroCorredor);
+            Atletas[i] = new Thread(new Atleta(postaCarrera, tiempoInicioCarrera),"Corredor " + numeroCorredor);
             Atletas[i].start();
         }
+
+       
     }
     
 }
